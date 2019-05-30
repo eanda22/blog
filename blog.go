37b74333dae2603674f83554ecb27c49d03b357c
eanda22 <46227705+eanda22@users.noreply.github.com>
2019-05-30 18:14:24 -0400

    Update blog.go

--- blog.go
+++ blog.go
@@ -4,11 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-    "encoding/json"
-    "io/ioutil"
-    "net/http"
+        "encoding/json"
+	"net/http"
+	"log"
+	"io/ioutil"
 )
 
+type blog struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
 
 var all_blogs[] string
 
@@ -19,48 +24,49 @@ func main() {
 	// post request
 	http.HandleFunc("/post", post_request)
 
-	// opens the server at port 80
-	http.ListenAndServe(":80", nil)
+	// opens the server at port 8080
+	http.ListenAndServe(":8080", nil)
 }
 
 func get_request(w http.ResponseWriter, r *http.Request) {
-	// gets the json data
-	json_body, err := json.Marshal(all_blogs)
+	// reads out the request into a slice of bytes
+	post, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Error", http.StatusInternalServerError)
 	}
-	w.Write(json_body)
+	// appends the post to the existing array of posts (as strings) and prints them all
+	all_blogs = append(all_blogs, string(post))
+	for i := 0; i < len(all_blogs); i++ {
+		fmt.Fprintf(w, all_blogs[i])
+	}
 	/*
-	*  add_entry() must be called to create the database
-	*  Not sure how to put this JSON data into the database
-	*/
+	 * Not sure how to take the slice of strings and put it into the database
+	 */
 }
 
 func post_request(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		// reads out the request into a slice of bytes
-		post, err := ioutil.ReadAll(r.Body)
+		decoder := json.NewDecoder(r.Body)
+		var post blog
+		err := decoder.Decode(&post)
 		if err != nil {
-			http.Error(w, "Error", http.StatusInternalServerError)
+			log.Fatal(err)
 		}
-		// appends the post to the existing array of posts (as strings) and prints them all
-		all_blogs = append(all_blogs, string(post))
-		for i := 0; i < len(all_blogs); i++ {
-			fmt.Fprintf(w, all_blogs[i])
-		}
+		log.Println(post)
+		fmt.Fprintf(w, "Post Saved")
 	}
 }
 
-func add_entry(id int, title string, body string) {
+func add_entry(title string, body string) {
 	
 	// creates a new database and creates a table with an id, title, and body
 	database, _ := sql.Open("sqlite3", "./blogger.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS blog_entry (id INTEGER PRIMARY KEY, title TEXT, body TEXT)")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS blog_entry (title TEXT, body TEXT)")
 	statement.Exec()
 
 	//inserts the blog entry into the database
-	statement, _ = database.Prepare("INSERT INTO blog_entry (id, title, body) VALUES (?, ?, ?)")
-	statement.Exec(id, title, body)
+	statement, _ = database.Prepare("INSERT INTO blog_entry (title, body) VALUES (?, ?)")
+	statement.Exec(title, body)
 
 }
 
